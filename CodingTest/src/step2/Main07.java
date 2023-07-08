1d1b85968bc44c244b7120af2af1a48e4b79e736
suj0309 <suj0309@naver.com>
2023-07-08 12:42:32 +0900

    step2 완

--- CodingTest/src/step2/Main07.java
+++ CodingTest/src/step2/Main07.java
@@ -1,8 +1,6 @@
 package step2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Scanner;
 
 /*
  * 백준 2480 주사위 세계
@@ -15,33 +13,55 @@ import java.util.List;
  *  3개 주사위의 나온 눈이 주어질 때 상금 계산 프로그램 작성
  *  
  *  입력 3 3 6	| 2 2 2	| 6 2 5
- *  출력 1300	| 12000 | 600
+ *  출력 1300		| 12000 | 600
  */
 
 public class Main07 {
 
-	public static void main(String[] args) {
-		
-		List<Integer> list = new ArrayList<>();
-		for(int i=0; i<3; i++) {
-			list.add((int)(Math.random()*6)+1);
-		}
-//		System.out.println(list.get(0));
-//		System.out.println(list.get(1));
-//		System.out.println(list.get(2));
-		
-		// 주사위값은 넣었어
-		// 이걸 오름차순으로 꺼내
-		int a = 0;
-		int b = 0;
-		int c = 0;
-		
-		if(a == b) {
-			if (b == c) System.out.println(10000+(c*1000));
-			else System.out.println(1000+(b*100));
-		} else if(a == c) {
-			
+	public static void xyz(int x, int y, int z) {
+		if (x == y)
+			System.out.println(10000 + (x * 1000));
+		else if(x == z)
+			System.out.println(1000 + (x * 100));
+		else
+			System.out.println(1000 + (y * 100));
+	}
+
+	public static void abc(int a, int b, int c) {
+		if (a != b && a != c && b != c) {
+			if (a > b && a > c)
+				System.out.println(a * 100);
+			else if (b > a && b > c)
+				System.out.println(b * 100);
+			else
+				System.out.println(c * 100);
 		}
 	}
 
+	public static void main(String[] args) {
+
+		// random으로 받지으면 안되고, 입력해서 받아야된다고함
+//		List<Integer> list = new ArrayList<>();
+//		for(int i=0; i<3; i++) {
+//			list.add((int)(Math.random()*6)+1);
+//		}
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+//		System.out.printf("%s %s %s ", a,b,c);
+
+		if (a == b)
+			xyz(b, c ,a);
+		else if (a == c)
+			xyz(a, b, c);
+		else if (b == c)
+			xyz(a, c, b);
+		else
+			abc(a, b, c);
+
+		sc.close();
+
+	} // main end
 }
